8917ae0cb4d22acaf11101cda1d172b796e0fcbf
Joy Brar <joybrar2001@gmail.com>
2024-01-05 09:58:31 +0530

    Added Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <h1 className='text-bold text-teal-500 text-2xl'>
-      Hello World
-    </h1>
+    <>
+      <Hero />
+    </>
   );
 }
